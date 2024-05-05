524121eb94dab879fc3f3a83e9127c110f02b939
chika_takami <lushuyu-chan@qq.com>
2024-05-05 23:17:28 +0800

    update

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import React from 'react'
+import { Card, CardHeader } from '@nextui-org/react'
+import Link from 'next/link'
+import { Star, MessageSquareHeart } from 'lucide-react'
 import { ImageHandleProps, ImageType } from '~/types'
 import PhotoAlbum from 'react-photo-album'
 import { Button, Image, Spinner } from '@nextui-org/react'
@@ -29,6 +32,28 @@ export default function Masonry(props : Readonly<ImageHandleProps>) {
 
   return (
     <div className="w-full sm:w-4/5 mx-auto p-2">
+            {/* <Card isBlurred shadow="sm" className="h-48 items-center">
+        <CardBody className="flex flex-col space-y-2 ">
+          <span className="flex items-center">
+          <span className="h-px flex-1 bg-black"></span>
+          <span className="pr-6" style={{paddingLeft:"1.5em"}}>为世界上所有的美好而战！</span>
+          <span className="h-px flex-1 bg-black"></span>
+        </span>
+
+        </CardBody>
+      </Card> */}
+      <Card shadow="sm"  style={{marginBottom:"1em", fontFamily:"'Noto Serif SC', serif, system-ui"}}>
+        <CardHeader className="flex flex-col space-y-2">
+          <div className="flex gap-5">
+            <div className="flex flex-col gap-1 items-center ">
+            <span className="pr-6" style={{paddingLeft:"1.5em", fontSize:"40px"}}>Lu Shuyu's Gallery</span>
+              <span className="pr-6" style={{paddingLeft:"1.5em", fontSize:"20px"}}>为世界上所有的美好而战！</span>
+          <span className="h-px flex-1 bg-black"></span>
+            </div>
+          </div>
+
+        </CardHeader>
+      </Card>
       <PhotoAlbum
         columns={(containerWidth) => {
           if (containerWidth < 640) return 1;
@@ -65,7 +90,7 @@ export default function Masonry(props : Readonly<ImageHandleProps>) {
       <div className="flex items-center justify-center my-4">
         {
           isValidating ?
-            <Spinner label="Loading..." color="primary" />
+            <Spinner label="为世界上所有的美好而战！" color="primary" />
             :
           size < pageTotal &&
             <Button
